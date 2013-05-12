ff01b33e6024b7a948ee6310d41ca30e3502c5ee
Tom Prince <tom.prince@ualberta.net>
2013-05-12 06:56:44 -0700

    Split up monitor and hook-server code into seperate files.

---
+++ monitor.py
@@ -0,0 +1,21 @@
+from twisted.python import log
+from twisted.web.client import getPage
+from twisted.application import service
+
+from twisted.application.internet import TimerService
+
+class TracMonitor(service.MultiService):
+    def __init__(self, restartCallback, checkInterval, tracURL, timeout):
+        self._restartCallback = restartCallback
+        self._tracURL = tracURL
+        self._timeout = timeout
+        TimerService(checkInterval, self.check, tracURL, timeout).setServiceParent(self)
+
+    def restart(self, f):
+        log.err(f, 'accessing trac.')
+        log.msg('restarting trac.')
+        self._restartCallback()
+
+    def check(self):
+        return (getPage(self._tracURL, timeout=self._timeout)
+                .addErrback(self.restart))
