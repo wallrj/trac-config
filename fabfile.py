b912a53ebcfae0c2f10293383da0d7c424d6ba5d
Tom Prince <tom.prince@ualberta.net>
2013-05-31 00:53:08 -0600

    Move trac templates to trac-config from t-web.

--- fabfile.py
+++ fabfile.py
@@ -33,9 +33,6 @@ class Trac(service.Service):
             run('/bin/ln -nsf ~/attachments {}/trac-env/attachments'.format(
                 self.configDir))
 
-            run('/bin/ln -nsf ~/website/trac-files/templates {}/trac-env/templates'.format(
-                self.configDir))
-
             run('/bin/ln -nsf {} {}/trac-env/log'.format(self.logDir, self.configDir))
 
             run('/bin/ln -nsf {}/start {}/start'.format(self.configDir, self.binDir))
